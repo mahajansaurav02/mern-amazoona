19bc8f4209b988e5c2bd34f1ec1f84aada41ed00
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-01 21:00:03 +0530

    add product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
-;
+
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
-     <a href="/">amazoona</a>
+     <Link to="/">amazoona</Link>
       </header>
-      <main>list products</main>
+      <main>
+        <Routes>
+        <Route  path='/product/:slug' element={<ProductScreen/>} />
+        <Route  path='/' element={<HomeScreen/>} />
+        </Routes>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
