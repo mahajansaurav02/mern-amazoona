7b6f157efaa022a71c0c75718279468d2d167ac1
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-02 20:59:43 +0530

    Add Rating and add to cart button

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,67 @@
-import { Link } from 'react-router-dom';
-import data from './../data';
-function HomeScreen(){
-    return <div>
-         <h1>Featured Products</h1>
-        <div className="products"> 
-        {
-          data.products.map((product)=>
-          ( <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name}/>
-            </Link>
-            <div className="product-info">
-            <Link to={`/product/${product.slug}`}>
-            <p>{product.name} </p>
-            </Link>
-            <p><strong>₹{product.price}</strong></p>
-            <button>Add to cart</button>
-            </div>
-           
-          </div>
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+import logger from "use-reducer-logger";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Product from '../components/Product';
+// import data from './../data';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+      // setProducts(result.data)
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+          {products.map((product) => (
+            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+            <Product product={product}></Product>
+            </Col> 
           ))
-        }
-        </div>
+}
+          </Row>
+        )}
+        
+      </div>
     </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
