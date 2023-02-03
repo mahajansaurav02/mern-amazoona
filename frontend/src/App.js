7b093e51de2580cc49364427cc54453bd2ba0d46
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-03 18:00:03 +0530

    add code of add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,51 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+// import  Badge  from 'react-bootstrap/Badge';
+import { Store } from "./Store";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
+      <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>amazoona</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItem.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItem.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
-              <Route path='/product/:slug' element={<ProductScreen />} />
-              <Route path='/' element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
-
         </main>
         <footer>
-          <div className='text-center'>All rights reserved</div>
+          <div className="text-center">All rights reserved</div>
         </footer>
       </div>
     </BrowserRouter>
