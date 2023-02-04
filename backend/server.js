b98041f0aa5b4f97d8483ad2912c6f1d567f0633
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-04 23:00:06 +0530

    add authentication token code

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config()
 
@@ -14,13 +15,19 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 })
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use('/api/seed',seedRouter)
 app.use('/api/products',productRouter)
+app.use('/api/users',userRouter)
 
-
+app.use((err,req,res,next)=>{
+  res.status(5000).send({message:err.message})
+})
 
 const port = process.env.PORT || 5000
-
-app.listen(port, () => {
+app.listen(port, () => {-
   console.log("Express running on port " + port)
 })
