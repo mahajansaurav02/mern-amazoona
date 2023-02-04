523a4700450b60d3c041ff752c9a6d93857d4c05
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-04 15:38:40 +0530

    add create user Code

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -3,11 +3,14 @@ import data from "../data.js";
 const seedRouter = express.Router();
 
 import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 
 seedRouter.get("/", async (req, res) => {
- const remo= await Product.remove({});
+ await Product.remove({});
   const createProducts = await Product.insertMany(data.products);
-  res.send({ createProducts });
+ await User.remove({});
+  const createUser = await User.insertMany(data.users);
+  res.send({ createProducts,createUser });
 });
 
 export default seedRouter;
