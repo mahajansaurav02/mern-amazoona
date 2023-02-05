8d0e81344353f41e5a962e65c678fc4c2678591c
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-05 21:09:58 +0530

    complete signIn code

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,8 +10,7 @@ const user=await User.findOne({email:req.body.email})
 
 if(user){
     if(bcrypt.compareSync(req.body.password,user.password)){
-
-
+        
         res.send({
             _id:user._id,
             name:user.name,
