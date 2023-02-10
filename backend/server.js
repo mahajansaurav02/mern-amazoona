c81e0c434049c08c239a2bbe6572c30cbf7d55fd
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-10 21:16:32 +0530

    Add for diployment

--- backend/server.js
+++ backend/server.js
@@ -18,21 +18,28 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/keys/paypal',(req,res)=>{
+app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 })
-app.use('/api/seed',seedRouter)
-app.use('/api/products',productRouter)
-app.use('/api/users',userRouter)
-app.use('/api/orders',orderRouter)
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
 
-app.use((err,req,res,next)=>{
-  res.status(5000).send({message:err.message})
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
+app.use((err, req, res, next) => {
+  res.status(5000).send({ message: err.message })
 })
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {-
-  console.log(`serve at http://localhost:${port}`)
+app.listen(port, () => {
+  -
+    console.log(`serve at http://localhost:${port}`)
 })
