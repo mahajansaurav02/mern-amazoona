3680e7ecf9315a6da26d28de30099e4cf4ab180d
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-10 14:21:23 +0530

    addd homescreen issue

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,12 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import axios from "axios";
-import logger from "use-reducer-logger";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Product from '../components/Product';
-// import data from './../data';
-import { Helmet } from 'react-helmet-async';
-import LoadingBox from './../components/LoadingBox';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+// import data from '../data';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -23,23 +22,23 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: "",
   });
-  // const [products, setProducts] = useState([])
-
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
         const result = await axios.get("/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-      } catch (error) {
-        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
-      // setProducts(result.data)
+
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -51,23 +50,20 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <LoadingBox/>
+          <LoadingBox />
         ) : error ? (
-<MessageBox variant="danger">{error}</MessageBox>
-          ) : (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
           <Row>
-          {products.map((product) => (
-            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-            <Product product={product}></Product>
-            </Col> 
-          ))
-}
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
           </Row>
         )}
-        
       </div>
     </div>
   );
 }
-
 export default HomeScreen;
