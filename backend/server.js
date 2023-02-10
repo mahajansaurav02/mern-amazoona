a5e9cd967a1e613058b4da3313bb8808bfb58594
mahajansaurav02 <mahajansaurav02@gmail.com>
2023-02-10 20:56:04 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,6 @@ app.use(express.urlencoded({extended:true}))
 app.get('/api/keys/paypal',(req,res)=>{
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 })
-
-const __dirname=path.resolve()
-app.use(express.static(path.join(__dirname,'/frontend/build')))
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/frontend/build/index.html'))
-})
-
-
 app.use('/api/seed',seedRouter)
 app.use('/api/products',productRouter)
 app.use('/api/users',userRouter)
@@ -42,5 +34,5 @@ app.use((err,req,res,next)=>{
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {-
-  console.log("Express running on port " + port)
+  console.log(`serve at http://localhost:${port}`)
 })
