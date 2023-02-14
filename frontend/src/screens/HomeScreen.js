b72fd94db4fcd054cd83876a99f87ccbdc55e968
Saurav Sunil Mahajan <110589554+mahajansaurav02@users.noreply.github.com>
2023-02-14 09:56:27 +0530

    Change amazoona name

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>BhumizCart</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
